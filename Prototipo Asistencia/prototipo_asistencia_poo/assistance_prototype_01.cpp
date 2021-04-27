c472d951f7e0911d1ac4aa7bf4e2af752f7246e1
Ikiusen <thiagofcabrera00@gmail.com>
2021-04-27 20:47:53 -0300

    Avances en el prototipo 01

--- Prototipo Asistencia/prototipo_asistencia_poo/assistance_prototype_01.cpp
+++ Prototipo Asistencia/prototipo_asistencia_poo/assistance_prototype_01.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 class Student
 {
@@ -96,6 +97,11 @@ public:
 		return this->date;
 	}
 
+	void setState(bool state)
+	{
+		this->state = state;
+	}
+
 	bool getState()
 	{
 		return this->state;
@@ -105,62 +111,115 @@ public:
 class AssitanceManagerMapper
 {
 private:
-	
+	std::vector<AssistanceManager*> assistanceManagers;
 public:
-	AssitanceManagerMapper(){};
+	AssitanceManagerMapper()
+	{
+	}
 		
-
 	~AssitanceManagerMapper(){};
 
-	void insert(AssistanceManager* assitanceManager)
+	void add(AssistanceManager* assistanceManager)
+	{
+		assistanceManagers.push_back(assistanceManager);
+	}
+
+	void deleteAssitanceRegistry(AssistanceManager* assistanceManager)
+	{
+		assistanceManagers.erase(std::remove(assistanceManagers.begin(), assistanceManagers.end(), assistanceManager), assistanceManagers.end());
+	}
+
+	void updateAssitanceRegistry(AssistanceManager* assistanceManagerToUpdate,bool state)
+	{
+		for(AssistanceManager* assistanceManager : assistanceManagers)
+            	{
+                	if(assistanceManager != 0)
+                	{
+	                   if((assistanceManager->getStudent().getName() == assistanceManagerToUpdate->getStudent().getName())
+	                   	&&(assistanceManager->getStudent().getSurname() == assistanceManagerToUpdate->getStudent().getSurname())
+	                   	&&(assistanceManager->getCourse().getName() == assistanceManagerToUpdate->getCourse().getName())
+	                   	&&(assistanceManager->getDate()==assistanceManagerToUpdate->getDate()))
+	                   {
+	                   	assistanceManager->setState(state);
+	                   }
+                	}
+           		}
+    }
+
+	void insertToFile()
 	{
 		std::ofstream saveFile;
     	saveFile.open("assistance_registry.txt", std::ios::in | std::ios::out | std::ios::app);
 	    if(saveFile.is_open())
 	    {
-	             saveFile << assitanceManager->getStudent().getName() <<" "<< assitanceManager->getStudent().getSurname() <<" "<< assitanceManager->getCourse().getName() <<" "<<
-	             assitanceManager->getDate() <<" "<< assitanceManager->getState() << std::endl;
+	    	for(AssistanceManager* assistanceManager : assistanceManagers)
+            	{
+                	if(assistanceManager != 0)
+                	{
+	                   saveFile << assistanceManager->getStudent().getName() <<" "<< assistanceManager->getStudent().getSurname() <<" "<< assistanceManager->getCourse().getName() <<" "<<
+		               assistanceManager->getDate() <<" "<< assistanceManager->getState() << std::endl;
+                	}
+           		}
+	             
 	    }
 	    saveFile.close();
 	}
 
-	void update(AssistanceManager* assitanceManager,bool state)
-	{
-
-	}
-
-	void deleteAssitanceRegistry(AssistanceManager* assitanceManager)
-	{
-
-	}
 };
 
 
 
 int main()
 {
-	//Student* student1 = new Student("Thiago","Cabrera");
-	Student* student1 = new Student("Javier","Cabrera");
+	Student* student1 = new Student("Thiago","Cabrera");
 
-	//Student* student2 = new Student("Laureano","Cabrera");
-	Student* student2 = new Student("Gabriela","Bosco");
-	//Course* course1 = new Course("Practicas Profesionalizantes");
-	Course* course1 = new Course("Analisis Matematico");
+	Student* student2 = new Student("Javier","Cabrera");
 
-	AssistanceManager* assistance = new AssistanceManager(student1,course1,"20210422",true);
+	Student* student3 = new Student("Laureano","Cabrera");
 
-	AssistanceManager* assistance2 = new AssistanceManager(student2,course1,"20210422",false);
+	Student* student4 = new Student("Gabriela","Bosco");
+
+	Course* course1 = new Course("Practicas Profesionalizantes");
+
+	Course* course2 = new Course("Analisis Matematico");
+
+	AssistanceManager* assistance1 = new AssistanceManager(student1,course1,"20210422",false);
+
+	AssistanceManager* assistance2 = new AssistanceManager(student2,course2,"20210422",false);
+
+	AssistanceManager* assistance3 = new AssistanceManager(student3,course2,"20210422",false);
+
+	AssistanceManager* assistance4 = new AssistanceManager(student4,course1,"20210422",false);
 
 	AssitanceManagerMapper* assistanceMapper = new AssitanceManagerMapper();
 
-	assistanceMapper->insert(assistance);
+	assistanceMapper->add(assistance1);
 
-	assistanceMapper->insert(assistance2);
+	assistanceMapper->add(assistance2);
+
+	assistanceMapper->add(assistance3);
+
+	assistanceMapper->add(assistance4);
+
+	assistanceMapper->updateAssitanceRegistry(assistance2,true);
+
+	assistanceMapper->updateAssitanceRegistry(assistance4,true);
+
+	assistanceMapper->deleteAssitanceRegistry(assistance2);
+
+	assistanceMapper->insertToFile();
+
 
 	delete student1;
 	delete student2;
+	delete student3;
+	delete student4;
 	delete course1;
-	delete assistance;
+	delete course2;
+	delete assistance1;
+	delete assistance2;
+	delete assistance3;
+	delete assistance4;
 	delete assistanceMapper;
 
 	return 0;
